de9b27a87d0e518d132bc945bd757754c227f029
th0rin-idm <127588431+th0rin-idm@users.noreply.github.com>
2023-09-27 01:21:03 -0600

    File and parsing fragmentation in interface.pl

--- interface.pl
+++ interface.pl
@@ -1,36 +1,54 @@
-consult('./splitter.pl').
-consult('./parser.pl').
-
-:- use_module('./splitter.pl', [separar_cadena/3]).
-
-
-
+:-consult(splitter).
+:-consult(originParser).
+:-consult(destinyParser).
+:-consult(intermediateParser).
 
 %------------------------WELCOME
 heywazelog :-
-    write('Hola usuario, donde se encuentra ubicado actualmente?.'), nl,
-    read_string(user_input, "\n",  "\r", _, Start),
-    write('Usted esta en '), write(Start), nl,
+    nl, write('Hola usuario, bienvenido'), nl,
+    getOrigin.
+ 
+%------------------------DATA COLLECTION
+getOrigin:-
+    nl,write('Donde se encuentra ubicado actualmente?.'), nl,
+    read_string(user_input, "\n",  "\r", _, START),
+    split_string(START, ' ', SUBLIST_START), 
+    (origin(SUBLIST_START) -> getDestination ; originError).
+    
+getDestination:-
+    nl,write('Favor ingresa tu destino.'), nl,
+    read_string(user_input, "\n", "\r", _, END),
+    split_string(END, ' ', SUBLIST_END), 
+    (destiny(SUBLIST_END) -> continueConversation ; destinyError).
 
-    write('Favor ingrese su destino.'), nl,
-    read_string(user_input, "\n", "\r", _, End),
-    write('Usted va para '), write(End), nl, 
-    continueconversation.
+continueConversation :-
+    nl,write('Deseas agregar un punto intermedio?'), nl, 
+    read_string(user_input, "\n", "\r", _, ANSWER),
+    (ANSWER = "si" -> addIntermediate ; endConversation).
 
-continueconversation :-
-    write('Desea agregar un punto intermedio?'), nl,
-    read_string(user_input, "\n", "\r", _, Answer),
-    (Answer = "si" -> addintermediate ; endconversation).
+addIntermediate:-
+    nl,write('Ingresa el punto intermedio o escribe no para terminar.'), nl,
+    read_string(user_input, "\n", "\r", _, INTERMEDIATE),
+    split_string(INTERMEDIATE, ' ', SUBLIST_INTER), 
+    (INTERMEDIATE = "no" -> endConversation ; (inter(SUBLIST_INTER) -> continueConversation ; intermediateError)).
+    
+endConversation :-
+    nl,write('Deseas terminar la conversacion? (si/no)'), nl,
+    read_string(user_input, "\n", "\r", _, TERMINATE),
+    nl,(TERMINATE = "si" -> write('Gracias por usar el programa. Conversacion terminada.'), nl, nl ; continueConversation).
 
-addintermediate :-
-    write('Ingrese el punto intermedio o escriba no para terminar.'), nl,
-    read_string(user_input, "\n", "\r", _, Intermediate),
-    (Intermediate = "no" -> endconversation ; continueconversation).
+%------------------------errorManager
+originError:-
+    nl, write("El origen ingresado no se encuentra en mi base de datos o no comprendo tu entrada, favor ingresa una distinta."),nl,
+    getOrigin.
 
-endconversation :-
-    write('Desea terminar la conversacion? (si/no)'), nl,
-    read_string(user_input, "\n", "\r", _, Terminate),
-    (Terminate = "si" -> write('Gracias por usar el programa. Conversacion terminada.'), nl ; continueconversation).
+destinyError:-
+    nl, write("El destino ingresado no se encuentra en mi base de datos o no comprendo tu entrada, favor ingresa una distinta."),nl,
+    getDestination.
 
+intermediateError:-
+    nl, write("El destino ingresado no se encuentra en mi base de datos o no comprendo tu entrada, favor ingresa una distinta."),nl, 
+    addIntermediate.
+    
 
 
