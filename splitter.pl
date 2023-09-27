9f543afbaebd57d051cbee285a31c969ed8f0f6b
th0rin-idm <127588431+th0rin-idm@users.noreply.github.com>
2023-09-27 01:24:11 -0600

    Logic development of user's input analysis

--- splitter.pl
+++ splitter.pl
@@ -1,19 +1,23 @@
-% Cargar la biblioteca necesaria
+% Load the required library
 :- use_module(library(strings)).
 
-% Definir una cadena
-cadena("Hola mundo de Gumball").
+% Define a string
+stringSample("Hola mundo de Gumball").
 
-% Separar la cadena utilizando el número 4 como separador
-separar_cadena(Cadena, Separador, ListaDeSubcadenas) :-
-    split_string(Cadena, Separador, "", Subcadenas), % Obtén las subcadenas sin comillas
+% Split the string using a splitter
+split_string(CHAIN, SPLITTER, SUBSTRING_LIST) :-
+    split_string(CHAIN, SPLITTER, "", SUBSTRINGS), % Obtén las subcadenas sin comillas
 
     % Envuelve cada subcadena en comillas y agrégala a la nueva lista
-    maplist(wrap_in_quotes, Subcadenas, ListaDeSubcadenas).
+    maplist(wrap_in_quotes, SUBSTRINGS, SUBSTRING_LIST).
 
 % Predicado auxiliar para envolver una subcadena en comillas
-wrap_in_quotes(Subcadena, SubcadenaConComillas) :-
-    string_concat('"', Subcadena, Temp), % Agrega comillas al principio
-    string_concat(Temp, '"', SubcadenaConComillas). % Agrega comillas al final
+wrap_in_quotes(SUBSTRING, SUBSTRING_MARK) :-
+    string_concat('', SUBSTRING, TEMP), % Agrega comillas al principio
+    string_concat(TEMP, '', SUBSTRING_MARK). % Agrega comillas al final
 
+ejemploSepararCadenas :-
+    split_string("Actualmente estoy ubicado en Puntarenas", " ", SUBSTRINGS),
+    write(SUBSTRINGS).
+%:- ejemploSepararCadenas.
 
