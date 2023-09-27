7554dac234576467735ffa22f8bae05d096c006c
yherlandelizondo <yherland25@gmail.com>
2023-09-27 14:10:50 -0600

    remove dataStructure.pl

--- dataStructure.pl
+++
@@ -1,8 +0,0 @@
-% Predicate to add an element to a list
-addElement(ELEMENT, ORIGINAL_LIST, NEW_LIST) :-
-    NEW_LIST = [ELEMENT | ORIGINAL_LIST].
-
-example:-
-    addElement(5, [1, 2, 3, 4], NEW_LIST),
-    write(NEW_LIST).
-%:-example.
