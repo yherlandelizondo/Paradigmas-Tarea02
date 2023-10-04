311b1c69625e65e1920fee5894611a39fb9c8afd
th0rin-idm <127588431+th0rin-idm@users.noreply.github.com>
2023-10-04 05:37:24 -0600

    Final Update interface.pl

--- interface.pl
+++ interface.pl
@@ -1,7 +1,8 @@
+:-consult('Logic/dijkstra2.pl').
 :-consult('Logic/splitter.pl').
-:-consult('expertSystem/parser.pl').
-:-consult('Logic/lastElem.pl').
-
+:-consult('ExpertSystem/parser.pl').
+:-consult('Logic/listManager.pl').
+:-consult('Logic/quicksort.pl').
 
 %-------------------------------------WELCOME
 heywazelog :-
@@ -14,31 +15,46 @@ getOrigin:-
     nl,write('Donde se encuentra ubicado actualmente?.'), nl,
     read_string(user_input, "\n",  "\r", _, START),
     split_string(START, ' ', SUBLIST_START), 
-    (analyzeSentence(SUBLIST_START) -> getDestination ; originError).
+    lastElement(SUBLIST_START, LASTITEM_START), 
+    (analyzeSentence(SUBLIST_START) -> getDestination(LASTITEM_START) ; originError).
     
-getDestination:-
+getDestination(START):-
+    %write(START),nl,
     nl,write('Favor ingresa tu destino.'), nl,
     read_string(user_input, "\n", "\r", _, END),
     split_string(END, ' ', SUBLIST_END), 
-    (analyzeSentence(SUBLIST_END) -> addIntermediate([]) ; destinyError).
+    lastElement(SUBLIST_END, LASTITEM_END), 
+    %write(LASTITEM_END),nl,
+    (analyzeSentence(SUBLIST_END) -> addIntermediate(START,[],LASTITEM_END) ; destinyError).
 
 
-addIntermediate(INTERMEDIATE_LIST):-
+addIntermediate(START,INTERMEDIATE_LIST,END):-
+    %write(END),nl,
     nl,write('Okay. Ingresa un punto intermedio o escribe "ya" para terminar.'), nl,
     read_string(user_input, "\n", "\r", _, INTERMEDIATE),
     split_string(INTERMEDIATE, ' ', SUBLIST_INTER), 
-        (INTERMEDIATE = "ya" -> answer(INTERMEDIATE_LIST) 
+        (INTERMEDIATE = "ya" -> answer(START,INTERMEDIATE_LIST,END)
         ; 
-            (analyzeSentence(SUBLIST_INTER) -> lastElement(SUBLIST_INTER, LASTITEM), append(INTERMEDIATE_LIST, [LASTITEM], NEW_INTERMEDIATE_LIST), 
-            addIntermediate(NEW_INTERMEDIATE_LIST))
+            (analyzeSentence(SUBLIST_INTER) -> 
+                lastElement(SUBLIST_INTER, LASTITEM), 
+                %write(LASTITEM),write('m'),nl,
+                route(START,LASTITEM,TIME,RUTE),
+                %write(START),write('m'),write(LASTITEM),write('m'), %todo bien
+                %write(TIME),nl,
+                %write(RUTE),nl,
+                add_value_to_elements([LASTITEM], TIME, ITEMNUM),
+                %write([LASTITEM]),write('m'),write(TIME),write('m'),write(ITEMNUM),write('m'),nl,
+                append(INTERMEDIATE_LIST, ITEMNUM, NEW_INTERMEDIATE_LIST), 
+                write(NEW_INTERMEDIATE_LIST),
+                addIntermediate(START,NEW_INTERMEDIATE_LIST,END))
             ; 
-            (analyzeAuxiliarSentence(SUBLIST_INTER) -> interDirectioner(INTERMEDIATE_LIST) 
+            (analyzeAuxiliarSentence(SUBLIST_INTER) -> interDirectioner(START,INTERMEDIATE_LIST,END))   
                 %aqui llamamos a los predicados correspondientes que preguntan cual y donde
                 
             ; %si no se cumple, llama error
-            intermediateError(INTERMEDIATE_LIST))).
+            intermediateError(INTERMEDIATE_LIST)).
 
-interDirectioner(INTERMEDIATE_LIST) :-
+interDirectioner(START,INTERMEDIATE_LIST,END) :-
     nl,write("Cual?"), nl,
     read_string(user_input, "\n", "\r", _, SITUATION), 
     nl, write("Indicame donde queda "),
@@ -46,22 +62,48 @@ interDirectioner(INTERMEDIATE_LIST) :-
     write("?"), nl,
     read_string(user_input, "\n", "\r", _, SITUATION_INTERMEDIATE),
     split_string(SITUATION_INTERMEDIATE, ' ', SUBLIST_SITUATION), 
-    (analyzeSentence(SUBLIST_SITUATION) -> lastElement(SUBLIST_SITUATION, LASTITEM), append(INTERMEDIATE_LIST, [LASTITEM], NEW_INTERMEDIATE_LIST),
-    addIntermediate(NEW_INTERMEDIATE_LIST)
+
+    (analyzeSentence(SUBLIST_SITUATION) -> 
+        lastElement(SUBLIST_SITUATION, LASTITEM), 
+
+        route(START,LASTITEM,TIME,RUTE),
+        %write(START),write('m'),write(LASTITEM),write('m'), %todo bien
+        add_value_to_elements([LASTITEM], TIME, ITEMNUM),
+        %write(ITEMNUM),write('m'),nl,
+
+        append(INTERMEDIATE_LIST, ITEMNUM, NEW_INTERMEDIATE_LIST),
+
+        addIntermediate(START,NEW_INTERMEDIATE_LIST, END)
     ;
-    intermediateError(INTERMEDIATE_LIST)).
+    intermediateError(START,NEW_INTERMEDIATE_LIST, END)).
     
-answer(INTERMEDIATE_LIST) :-
-    nl,write('Tu ruta a seguir es la siguiente'), nl,
-    write(INTERMEDIATE_LIST),
-    nl,write('Con un tiempo estimado de '), 
-    write(" minutos"),
-    (endConversation).
+answer(START,INTERMEDIATE_LIST,END) :-
+    quicksort(INTERMEDIATE_LIST, SORTEDLIST),
+    firstExtractor(SORTEDLIST,FINALINTER_LIST),
+
+    add_to_beginning(START, FINALINTER_LIST, START_INTER),
+    add_to_end(END, START_INTER, START_INTER_END),
+
+    calculate_total_duration_and_route(START_INTER_END, TDURATION, TROUTE),
+    add_to_beginning(START, TROUTE, FINAL_ROUTE),
+
+    nl,write('Tu ruta a seguir es la siguiente:'), nl,
+    nl,write(FINAL_ROUTE),nl,
+    %write(INTERMEDIATE_LIST),write(SORTEDLIST),
+    nl,write('Con un tiempo estimado de: '),nl,nl, 
+    write(TDURATION),
+    write(" minutos en horario normal."),nl,
+    multiply_by_2(TDURATION,TDURATION_DAM),
+    write(TDURATION_DAM),
+    write(" minutos en hora pico."),nl,
+
+
+    (endConversation(START,SORTEDLIST,END)).
     
-endConversation:-
-    nl,write('Deseas terminar la conversacion? (si/no)'), nl,
+endConversation(START,SORTEDLIST,END):-
+    nl,write('Deseas terminar la conversacion? (si/no)'), nl, 
     read_string(user_input, "\n", "\r", _, TERMINATE),
-    nl,(TERMINATE = "si" -> write('Gracias por usar el programa. Conversacion terminada.'), nl, nl ; addIntermediate([])). 
+    nl,(TERMINATE = "si" -> write('Gracias por usar el programa. Conversacion terminada.'), nl, nl ; addIntermediate(START,SORTEDLIST,END)). 
 
 %-------------------------------------ERROR MANAGER
 originError:-
@@ -72,9 +114,9 @@ destinyError:-
     nl, write("El destino ingresado no se encuentra en mi base de datos o no comprendo tu entrada, favor ingresa una distinta."),nl,
     getDestination.
 
-intermediateError(INTERMEDIATE_LIST):-
+intermediateError(START,INTERMEDIATE_LIST,END):-
     nl, write("El destino ingresado no se encuentra en mi base de datos o no comprendo tu entrada, favor ingresa una distinta."),nl, 
-    addIntermediate(INTERMEDIATE_LIST).
+    addIntermediate(START,INTERMEDIATE_LIST,END).
     
 
 
