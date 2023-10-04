52765121481f52ab47ee6a83b62a1b81ce342fd5
th0rin-idm <127588431+th0rin-idm@users.noreply.github.com>
2023-10-04 05:33:44 -0600

    Update quicksort.pl

--- Logic/quicksort.pl
+++ Logic/quicksort.pl
@@ -13,12 +13,5 @@ partition([[Name1, Dist1]|Ys], [Name2, Dist2], Lows, [[Name1, Dist1]|Highs]) :-
     Dist1 > Dist2,
     partition(Ys, [Name2, Dist2], Lows, Highs).
 
-% Rule to get the first elemento of a list
-first_element([FIRST_ELEMENT|_], FIRST_ELEMENT).
-
-% Rule to get the last element of a list
-last_element([LAST], LAST).
-last_element([_|REST], LAST) :-
-last_element(REST, LAST).
 
 
